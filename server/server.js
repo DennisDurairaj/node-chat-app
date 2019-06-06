93e0c5e458e0c977f368d2834ca272b1acdd2988
Dennis Durairaj <dennis.durairaj@pl.pwc.com>
2019-06-06 14:17:31 +0200

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -1,12 +1,35 @@
 const path = require("path");
+const http = require('http');
 const express = require("express");
+const socketIO = require('socket.io');
+
 const port = process.env.PORT || 9001;
 
 const publicPath = path.join(__dirname, "../public");
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  
+  socket.emit('newMessage', {
+    from: 'John',
+    text: 'See you then',
+    createdAt: 123123
+  });
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });  
+});
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log("Server is up on port 9001");
 });
